db319bff2b0652c93ad0573cd17d9520c16b9c8f
younas <younaskk120@gmail.com>
2025-11-01 12:49:16 -0700

    smooth scrolling behavior

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" data-scroll-behavior="smooth">
       <head>
         <link
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
