f468978543f533594407347ddc0305dfee7a9b62
younas <younaskk120@gmail.com>
2025-11-01 13:26:58 -0700

    icons bug resolved

--- app/layout.js
+++ app/layout.js
@@ -72,19 +72,6 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
           rel="stylesheet"
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              if (document.fonts) {
-                document.fonts.ready.then(function() {
-                  document.querySelectorAll('.material-symbols-outlined').forEach(function(el) {
-                    el.classList.add('font-loaded');
-                  });
-                });
-              }
-            `,
-          }}
-        />
       </head>
       <body className={`${inter.variable} font-display antialiased`}>
         <ToastProvider>
